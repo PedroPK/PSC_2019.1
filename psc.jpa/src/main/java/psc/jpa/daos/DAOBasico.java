7406d5dd625f22c6c01f3964cfe330e255c4916e
pedropk <pedropk@gmail.com>
2019-02-24 01:05:52 -0300

    Learning how to use Logging with Log4J

--- psc.jpa/src/main/java/psc/jpa/daos/DAOBasico.java
+++ psc.jpa/src/main/java/psc/jpa/daos/DAOBasico.java
@@ -2,23 +2,35 @@ package psc.jpa.daos;
 
 import javax.persistence.EntityManager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import psc.jpa.entities.Correntista;
 import psc.jpa.utils.UtilJPA;
 
 public class DAOBasico {
 	
+	private Logger logger;
+	
 	public DAOBasico() {
-		//Logger logger = LoggerFactory.getLogger(name)
+		this.logger = LoggerFactory.getLogger("psc.jpa.daos.DAOBasico");
+		this.logger.info("DAOBasico instancializado");
 	}
 	
 	public void insert(Correntista pCorrentista) {
+		logger.debug("Método Inserir Correntista invocado");
+		
 		EntityManager em = UtilJPA.getEntityManager();
+		logger.debug("Entity Manager obtido com Sucesso");
 		
 		em.getTransaction().begin();
+		logger.debug("Transação aberta");
 		
 		em.persist(pCorrentista);
+		logger.debug("Persist invocado com Sucesso");
 		
 		em.getTransaction().commit();
+		logger.debug("Transação finalizada com Sucesso");
 	}
 	
 	public Correntista select(int pCodigoCorrentista) {
